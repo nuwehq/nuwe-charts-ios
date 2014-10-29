9df7a884821acf90a1476f6763bd9a1369ded19f
Dimitar Plamenov <dimitar.worldev@gmail.com>
2014-10-29 02:24:44 +0200

    Completed the Nuwe Score Chart Framework.

---
+++ TestNuweScoreFramework/NuweScore.framework/Versions/A/Headers/NuweScoreChart.framework/Headers/PNBar.h
@@ -0,0 +1,30 @@
+//
+//  PNBar.h
+//  PNChartDemo
+//
+//  Created by kevin on 11/7/13.
+//  Copyright (c) 2013年 kevinzhow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@protocol PNBarDelegate <NSObject>
+
+- (void) clickedBar:(id) bar;
+
+@end
+
+@interface PNBar : UIView
+
+@property (nonatomic) float grade;
+
+@property (nonatomic,strong) CAShapeLayer * chartLine;
+
+@property (nonatomic, strong) UIColor * barColor;
+
+@property (nonatomic, assign) id<PNBarDelegate> pDelegate;
+
+-(void)rollBack;
+
+@end
---
+++ TestNuweScoreFramework/NuweScoreChart.framework/Headers/PNLineChartData.h
@@ -0,0 +1,41 @@
+//
+// Created by Jörg Polakowski on 14/12/13.
+// Copyright (c) 2013 kevinzhow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/**
+ *  not support PNLineChartPointStyleTriangle style recently
+ */
+typedef NS_ENUM(NSUInteger, PNLineChartPointStyle) {
+    
+    PNLineChartPointStyleNone = 0,
+    PNLineChartPointStyleCycle,
+    PNLineChartPointStyleTriangle,
+    PNLineChartPointStyleSquare
+};
+
+@class PNLineChartDataItem;
+
+typedef PNLineChartDataItem *(^LCLineChartDataGetter)(NSUInteger item);
+
+@interface PNLineChartData : NSObject
+
+@property (strong) UIColor *color;
+@property NSUInteger itemCount;
+@property (copy) LCLineChartDataGetter getData;
+
+@property (nonatomic, assign) PNLineChartPointStyle inflexionPointStyle;
+
+/**
+ *  if PNLineChartPointStyle is cycle, inflexionPointWidth equal cycle's diameter
+ *  if PNLineChartPointStyle is square, that means the foundation is square with
+ *  inflexionPointWidth long
+ */
+@property (nonatomic, assign) CGFloat inflexionPointWidth;
+
+@property (nonatomic, assign) CGFloat lineWidth;
+
+@end
